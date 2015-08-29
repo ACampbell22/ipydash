cf799ae6e850810129cfcbac96849732cc9cbfaf
Andrew Campbell <acampbell@Andrews-MacBook-Air.local>
2015-08-29 14:59:46 +0200

    dockerized

--- application.py
+++ application.py
@@ -1,11 +1,20 @@
 from runipy.notebook_runner import NotebookRunner
 from IPython.nbformat import read, write
-# from IPython.nbconvert.exporters.html import HTMLExporter
 import nbformat
 from nbconvert import HTMLExporter
 
 from flask import Flask, render_template, Markup
+# from flask.ext.github import GitHub
 app = Flask(__name__)
+
+# app.config['GITHUB_CLIENT_ID'] = 'XXX'
+# app.config['GITHUB_CLIENT_SECRET'] = 'YYY'
+
+# # For GitHub Enterprise
+# app.config['GITHUB_BASE_URL'] = 'https://HOSTNAME/api/v3/'
+# app.config['GITHUB_AUTH_URL'] = 'https://HOSTNAME/login/oauth/'
+
+# github = GitHub(app)
 
 @app.route('/')
 def render_nb():
@@ -20,14 +29,12 @@ def render_nb():
 
 	html_exporter = HTMLExporter()
 	body, resources = html_exporter.from_notebook_node(nb)
-	# body = Markup.escape(body)
+
 	html_file= open("static/stock_infos.html","w")
 	html_file.write(body.encode('utf8', 'ignore'))
 	html_file.close()
 
-
-	# render_template('stock_infos.html')
 	return app.send_static_file('stock_infos.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
